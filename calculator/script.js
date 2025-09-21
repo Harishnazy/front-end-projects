4a1fb1d9c9b30c3194c0303a65d668e03a0b1fc3
Harishnazy <92583134+Harishnazy@users.noreply.github.com>
2025-09-21 18:07:49 +0530

    ADD : add css and js file

---
+++ calculator/script.js
@@ -0,0 +1,74 @@
+const currentDisplay = document.querySelector('.current-display');
+const previousDisplay = document.querySelector('.previous-display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operation');
+const equalsBtn = document.querySelector('.equals');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+let operation;
+
+function appendNumber(number) {
+    if (number === "." && currentDisplay.innerText.includes(".")) return;
+    currentDisplay.innerText += number;
+}
+
+function chooseOperation(operand) {
+    if (currentDisplay.innerText === "") return;
+    compute(operand);
+    operation = operand;
+    currentDisplay.innerText += operand;
+    previousDisplay.innerText = currentDisplay.innerText;
+    currentDisplay.innerText = "";
+}
+
+function clearDisplay() {
+    currentDisplay.innerText = "";
+    previousDisplay.innerText = "";
+}
+
+function compute(operand) {
+    let result;
+    const prev = parseFloat(previousDisplay.innerText);
+    const current = parseFloat(currentDisplay.innerText);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operand) {
+        case "+":
+            result = prev + current;
+            break;
+        case "-":
+            result = prev - current;
+            break;
+        case "*":
+            result = prev * current;
+            break;
+        case "/":
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentDisplay.innerText = result;
+}
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.innerText);
+    });
+});
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        chooseOperation(button.innerText);
+    });
+});
+
+equalsBtn.addEventListener('click', () => {
+    compute(operation);
+    previousDisplay.innerText = "";
+});
+
+clearBtn.addEventListener('click', clearDisplay);
+deleteBtn.addEventListener('click', () => {
+    currentDisplay.innerText = currentDisplay.innerText.slice(0, -1);
+}
+);
