6fc1a0034a4a9ff7d91ad3da761f8cea7b79a92d
Harishnazy <92583134+Harishnazy@users.noreply.github.com>
2025-11-21 18:14:13 +0530

    math number and string explain

---
+++ JavaScript/javascriptMath.js
@@ -0,0 +1,26 @@
+//  Calculations with Numbers and Strings
+const result = 5 + '10';
+
+console.log(result); // 510
+console.log(typeof result); // string
+
+const subtractionResult = '10' - 5;
+console.log(subtractionResult); // 5
+console.log(typeof subtractionResult); // number
+
+const multiplicationResult = '10' * 2;
+console.log(multiplicationResult); // 20
+console.log(typeof multiplicationResult); // number
+
+const divisionResult = '20' / 2;
+console.log(divisionResult); // 10
+console.log(typeof divisionResult); // number
+
+const result1 = null + 5;
+console.log(result1); // 5
+console.log(typeof result1); // number
+
+const result2 = undefined + 5;
+console.log(result2); // NaN
+console.log(typeof result2); // number
+
